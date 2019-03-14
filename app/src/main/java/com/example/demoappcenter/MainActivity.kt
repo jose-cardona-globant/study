89a1fe07cbac76ecab800c612151ace3037391b7
GLOBANT\jose.cardona <jose.cardona@globant.com>
2019-03-14 10:17:12 -0500

    added appcenter config

--- app/src/main/java/com/example/demoappcenter/MainActivity.kt
+++ app/src/main/java/com/example/demoappcenter/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.demoappcenter
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -25,8 +28,18 @@ class MainActivity : AppCompatActivity() {
         false
     }
 
+    private fun initAppCenter() {
+        AppCenter.start(
+            application,
+            "6db697cf-8dd3-41cf-9a47-355d3d7f9faf",
+            Analytics::class.java,
+            Crashes::class.java
+        )
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        initAppCenter()
         setContentView(R.layout.activity_main)
 
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
